d7c8beaa5411a87d358abd3f6dbeae5afde94225
Andrei Bonyhai <andrei.bonyhai@atta.systems>
2025-07-03 09:50:29 +0300

    base primeng theme + styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenubarModule, CardModule, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'deskbird-web';
+
+  get menubarItems() {
+    return [
+      { label: 'Home', icon: 'pi pi-home' },
+      { label: 'About', icon: 'pi pi-info-circle' }
+    ];
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,16 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideStore(), provideEffects()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideStore(),
+    provideEffects(),
+    providePrimeNG({ theme: { preset: Lara } })
+  ]
 };
